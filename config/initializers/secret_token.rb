b1d615eebeb3ded27fbe055586884b54de8b35fb
julio <julio336@hotmail.com>
2013-02-12 17:09:00 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chupandero::Application.config.secret_token = '186254f32d905d8da87a1294b110543f43931c7e87ab976fcfea4c8b7099606c97460c63463df9b4ef4a6986b83c4daba3eb3f90e0a4bbc2e366307e751306b7'
